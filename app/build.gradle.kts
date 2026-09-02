abc42e6b1f5c773e3cbecb2231c18d0f3796e934
SeruiSO <olars.m.1@gmail.com>
2026-09-02 13:44:59 +0300

    Fix Coil version 2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-    implementation("io.coil-kt:coil-compose:2.7.1")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)

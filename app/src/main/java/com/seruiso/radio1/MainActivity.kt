ceb3b46404f3febfd2f950dc1d5492258e17e95f
SeruiSO <olars.m.1@gmail.com>
2026-09-03 10:48:19 +0300

    Add detectDragGestures import

--- app/src/main/java/com/seruiso/radio1/MainActivity.kt
+++ app/src/main/java/com/seruiso/radio1/MainActivity.kt
@@ -37,6 +37,7 @@ import coil.compose.AsyncImage
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.ExperimentalFoundationApi

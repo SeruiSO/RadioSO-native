8ae8668d448591ea4fad867d51e5a0fa8f0d6bd2
SeruiSO <olars.m.1@gmail.com>
2026-09-03 15:22:58 +0300

    0.9.45: align versionName/versionCode with release tags (stack 0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 45
+        versionName = "0.9.45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

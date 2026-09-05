aa4b12160474bfe45cc046d8ab962bcea755f2bc
SeruiSO <olars.m.1@gmail.com>
2026-09-05 16:42:19 +0300

    0.9.84: update launcher icon (adaptive + densities)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 83
-        versionName = "0.9.83"
+        versionCode = 84
+        versionName = "0.9.84"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

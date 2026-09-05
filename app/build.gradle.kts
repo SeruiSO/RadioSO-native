139e0a07c735b2626610617603d3701b3d30fcf2
SeruiSO <olars.m.1@gmail.com>
2026-09-05 16:15:46 +0300

    0.9.83: new launcher icon RADIO SO (adaptive + all densities)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 82
-        versionName = "0.9.82"
+        versionCode = 83
+        versionName = "0.9.83"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

c6a070dd1e25c885da8f88052811d65984c21ee5
SeruiSO <Olars.m.1@gmail.com>
2026-09-06 17:59:56 +0300

    v0.9.96 versionCode/Name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 95
-        versionName = "0.9.95"
+        versionCode = 96
+        versionName = "0.9.96"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

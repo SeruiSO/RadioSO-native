703ee85603c1296e2e0911ccece56cdf29804973
SeruiSO <olars.m.1@gmail.com>
2026-09-09 10:48:06 +0300

    0.13.0: full current PC tree for review

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 146
-        versionName = "0.12.26"
+        versionCode = 150
+        versionName = "0.13.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

bbff57f1ebd51d1f516ad8c8201f595186f9415d
SeruiSO <olars.m.1@gmail.com>
2026-09-10 16:37:54 +0300

    0.13.50: official versionName/versionCode match APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 199
-        versionName = "0.13.49"
+        versionCode = 200
+        versionName = "0.13.50"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

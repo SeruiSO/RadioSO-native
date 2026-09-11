20f2a47882c2f200fe92bd08c1963bdc3bc705ee
SeruiSO <olars.m.1@gmail.com>
2026-09-11 18:03:25 +0300

    0.13.53: stations.json curated factory list from user settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 202
-        versionName = "0.13.52"
+        versionCode = 203
+        versionName = "0.13.53"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

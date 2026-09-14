a885c9a7caf80ad170ec4bce5d4943f28e081eea
SeruiSO <olars.m.1@gmail.com>
2026-09-14 17:16:58 +0300

    0.13.119: prune unused imports, orphan strings, old docs patches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 268
-        versionName = "0.13.118"
+        versionCode = 269
+        versionName = "0.13.119"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

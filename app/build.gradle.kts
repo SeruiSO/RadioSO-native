a013317ec4f85a9ca6cee0eb25077b51bd4765f2
SeruiSO <olars.m.1@gmail.com>
2026-09-14 14:04:03 +0300

    0.13.116: fix C13 UiHelpers import clip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 265
-        versionName = "0.13.115"
+        versionCode = 266
+        versionName = "0.13.116"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

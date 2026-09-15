d81e92b69ba6798ecf2d89e00ba03ad14530f2fd
SeruiSO <olars.m.1@gmail.com>
2026-09-15 15:52:18 +0300

    0.13.138: declare homeRailsToken for home-enter rails

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 287
-        versionName = "0.13.137"
+        versionCode = 288
+        versionName = "0.13.138"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

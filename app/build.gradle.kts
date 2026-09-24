8f3d7448947809fcd9f050e512f9faa09ab601ef
SeruiSO <olars.m.1@gmail.com>
2026-09-24 13:40:51 +0300

    0.13.221: replace dead favicon links

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 374
-        versionName = "0.13.220"
+        versionCode = 375
+        versionName = "0.13.221"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

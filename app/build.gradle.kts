0d81bc58915d694b7b5cdcbcb7af4184dab134c9
SeruiSO <olars.m.1@gmail.com>
2026-09-24 15:20:20 +0300

    0.13.224: version bump for info panel marquee

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 377
-        versionName = "0.13.223"
+        versionCode = 378
+        versionName = "0.13.224"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

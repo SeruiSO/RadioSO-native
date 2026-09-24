bf08c7025adb5aaf7ec654d36322da1a093d95b7
SeruiSO <olars.m.1@gmail.com>
2026-09-24 14:16:47 +0300

    0.13.222: replace dead and placeholder station icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 375
-        versionName = "0.13.221"
+        versionCode = 376
+        versionName = "0.13.222"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

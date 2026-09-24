3fba4ab58c260e6d3a0c47c8e379012e8c4fa9cb
SeruiSO <olars.m.1@gmail.com>
2026-09-24 12:35:33 +0300

    0.13.217: boxed catalog from radio_settings plus 12 techno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 370
-        versionName = "0.13.216"
+        versionCode = 371
+        versionName = "0.13.217"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

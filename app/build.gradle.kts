08c19e3db02b2c865dcfa18514beb1bdb0be57b1
SeruiSO <olars.m.1@gmail.com>
2026-09-24 12:58:21 +0300

    0.13.218: favicons for the 12 techno stations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.seruiso.radioso.nativeapp"
         minSdk = 26
         targetSdk = 37
-        versionCode = 371
-        versionName = "0.13.217"
+        versionCode = 372
+        versionName = "0.13.218"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
